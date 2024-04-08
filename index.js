ec9185da4dd6e46125182a7c01a599726b785887
Muhammad Nabil <realmuhammadnabil@gmail.com>
2024-04-09 03:54:15 +0700

    first init github

---
+++ index.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    var tries = 0;
+    const noButton = document.getElementById("button.no")
+    const yesButton = document.getElementById("button.yes")
+
+    noButton.addEventListener("mouseover", (event) => {
+        var x = event.pageX;
+        var y = event.pageY;
+
+        noButton.style.position = "absolute";
+        noButton.style.top = Math.random() * window.innerHeight +"px";
+        noButton.style.left = Math.random() * window.innerWidth + 'px';
+
+        tries++;
+
+        if(tries > 3){
+            yesButton.style.animation = "mover 0.8s infinite alternate"
+        }
+    });
+
+    yesButton.addEventListener("click", () => {
+        window.close();
+    });
+}
